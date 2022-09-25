ae8616d90a7c09b53b1355840a80fd0244d33e6f
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-09-25 15:18:00 +0100

    delete cards

--- app.js
+++ app.js
@@ -2,7 +2,18 @@ const welcomeAlert = document.querySelector('.welcome');
 
 const closeWelcome = document.querySelector('.fa-close');
 
+const deletebtns = document.querySelectorAll('.fa-trash');
+
+const flashcards = document.querySelectorAll('.flashcard')
+
 closeWelcome.addEventListener('click', ()=>{
     welcomeAlert.style.opacity = 0;
     setTimeout(() => {  welcomeAlert.style.display = 'none'; }, 400);
+});
+
+Array.from(deletebtns).forEach(btn =>{
+    btn.addEventListener('click', ()=>{
+        let card = btn.parentElement.parentElement;
+        setTimeout(() => {  card.style.display = 'none'; }, 400);
+    })
 });
