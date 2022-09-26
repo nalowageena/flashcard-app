28c1cf41bc0486506ed4371fa06e2a85163a99e9
Nalowa Geena <77391048+nalowageena@users.noreply.github.com>
2022-09-26 22:28:15 +0100

    save flashcards in an array

--- app.js
+++ app.js
@@ -1,36 +1,67 @@
 // DOM Element Selectors
-const welcomeAlert = document.querySelector('.welcome');
-const closeWelcome = document.querySelector('.fa-close');
-const deleteBtns = document.querySelectorAll('.fa-trash');
-const flashcards = document.querySelectorAll('.flashcard')
-const addFlashcardsBtn = document.querySelector('#add-flashcard-btn');
-const cardCreationModal = document.querySelector('#card-creation-modal');
-const cardCreationForm = document.querySelector('#card-creation-form');
-const modalCloseBtn = document.querySelector('#modal-close-btn');
+const welcomeAlert = document.querySelector(".welcome");
+const closeWelcome = document.querySelector(".fa-close");
+const deleteBtns = document.querySelectorAll(".fa-trash");
+const flashcards = document.querySelectorAll(".flashcard");
+const addFlashcardsBtn = document.querySelector("#add-flashcard-btn");
+const cardCreationModal = document.querySelector("#card-creation-modal");
+const cardCreationForm = document.querySelector("#card-creation-form");
+const modalCloseBtn = document.querySelector("#modal-close-btn");
+const cardForm = document.forms[0];
+
+const flashcardArray = [];
 
 // Event Listeners
-closeWelcome.addEventListener('click', removeWelcomeAlert); // It is better to seperate the event listeners from the functions so as to keep everything neat and organised - ONCE YOU'VE READ THIS, PLS DELETE THE COMMENT.
-addFlashcardsBtn.addEventListener('click', showCardCreationModal);
-cardCreationModal.addEventListener('click', closeCardCreationModal);
-cardCreationForm.addEventListener('click', e => {
+closeWelcome.addEventListener("click", removeWelcomeAlert);
+addFlashcardsBtn.addEventListener("click", showCardCreationModal);
+cardCreationModal.addEventListener("click", closeCardCreationModal);
+cardCreationForm.addEventListener("click", (e) => {
   e.stopPropagation(); // To prevent the modal from getting closed when the form is clicked
 });
-modalCloseBtn.addEventListener('click', closeCardCreationModal);
+modalCloseBtn.addEventListener("click", closeCardCreationModal);
+cardForm.addEventListener("submit", submitData);
 
 // Function to remove welcome alert when its close button is clicked
 function removeWelcomeAlert() {
   welcomeAlert.style.opacity = 0;
-  setTimeout(() => {  welcomeAlert.style.display = 'none'; }, 400);
+  setTimeout(() => {
+    welcomeAlert.style.display = "none";
+  }, 400);
 }
 
 // Function to show card creation modal
 function showCardCreationModal() {
-  cardCreationModal.style.display = 'flex';
+  cardCreationModal.style.display = "flex";
 }
 
 // Function to hide card creation modal
 function closeCardCreationModal() {
-  cardCreationModal.style.display = 'none';
+  cardCreationModal.style.display = "none";
+}
+
+// Function to create flashcard objects on submit
+function submitData(event) {
+  event.preventDefault(); //prevent the form from reloading after the submit button is clicked - which is the default behavior.
+  const cardData = new FormData(event.target);
+  const cardDataObj = new FLashcard();
+  cardData.forEach((value, key) => (cardDataObj[key] = value));
+  saveCard(cardDataObj);
+}
+
+// if any input field is left empty displays an error
+function formValidation(cardData) {
+  if (cardData.question == "" || cardData.answer == "") {
+    console.log("Error, invalid input");
+    return false;
+  }
+  return true;
+}
+
+// add card data to flashcard array then close the modal
+function saveCard(cardData) {
+  if (formValidation(cardData)) flashcardArray.push(cardData);
+  console.log(flashcardArray);
+  closeCardCreationModal()
 }
 
 // FOR NOW, LET'S DISABLE THE DELETE BTNS
@@ -40,3 +71,18 @@ function closeCardCreationModal() {
 //         setTimeout(() => {  card.style.display = 'none'; }, 400);
 //     })
 // });
+
+// flashcard constructor and prototype
+let FLashcard = function (question, answer) {
+  this.question = question;
+  this.answer = answer;
+};
+FLashcard.prototype = {
+  constructor: FLashcard,
+  getQuestion: function () {
+    return question;
+  },
+  getAnswer: function () {
+    return this.answer;
+  },
+};
